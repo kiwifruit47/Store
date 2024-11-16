5d9b712496c3e338e2a7776cab3070acc5ae67d2
kiwifruit47 <victoria.dimitrovax@icloud.com>
2024-11-16 19:52:56 +0200

    finished sale functionality and created issueReceipt method

--- src/main/java/org/citb408/Register.java
+++ src/main/java/org/citb408/Register.java
@@ -1,11 +1,16 @@
 package org.citb408;
 
+import org.citb408.exceptions.InsufficientAmountOfMoneyException;
+import org.citb408.util.*;
+
+import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 public class Register {
     private final int id;
+    private Store store;
     private Cashier cashier;
     private static int counter = 1;
 
@@ -13,8 +18,9 @@ public class Register {
         id = counter++;
     }
 
-    public Register(Cashier cashier) {
+    public Register(Cashier cashier, Store store) {
         this.cashier = cashier;
+        this.store = store;
         id = counter++;
     }
 
@@ -22,19 +28,61 @@ public class Register {
         return cashier;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
     public void setCashier(Cashier cashier) {
         this.cashier = cashier;
     }
 
     public BigDecimal calculatePriceForAmount(Product product, int amount) {
-
+        return store.getInventory().calculateSellingPrice(product).multiply(BigDecimal.valueOf(amount));
     }
 
-    public BigDecimal tagProductsFromCustomerCart (HashMap<Product, Integer> productsInCart) {
-        for (Map.Entry<Product, Integer> entry : productsInCart.entrySet()) {
+    public BigDecimal calculateTotalPriceForProductsInClientCart (Client client) {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Map.Entry<Product, Integer> entry : client.getProductsInCart().entrySet()) {
             Product product = entry.getKey();
             Integer amount = entry.getValue();
+            sum = sum.add(calculatePriceForAmount(product, amount));
+        }
+        return sum;
+    }
 
+    public void makeSale(Client client, BigDecimal money) throws InsufficientAmountOfMoneyException, IllegalStateException {
+        if (client.getProductsInCart().isEmpty()) {
+            throw new IllegalStateException("Client cart is empty.");
+        }
+        BigDecimal totalPrice = calculateTotalPriceForProductsInClientCart(client);
+        if (money.compareTo(totalPrice) < 0) {
+            throw new InsufficientAmountOfMoneyException("Insufficient amount of money! Sale cannot happen");
+        } else {
+            System.out.println("Payment successful!");
+            store.getInventory().removeProductsFromInventory(client.getProductsInCart());
         }
     }
+
+
+
+    public void issueReceipt(Client client, BigDecimal money) {
+        try {
+            makeSale(client, money);
+            Receipt receipt = new Receipt(this.cashier, LocalDateTime.now(), client.getProductsInCart(), calculateTotalPriceForProductsInClientCart(client));
+            System.out.println(receipt);
+            try {
+                Serializer.serialize("receipts/" + receipt.getReceiptId() + ".ser", receipt);
+            } catch (IOException e) {
+                System.out.println("Failed to serialize receipt." + e.getMessage());
+            }
+        } catch (InsufficientAmountOfMoneyException | IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
 }

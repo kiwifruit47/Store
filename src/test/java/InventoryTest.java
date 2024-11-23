53460d1a5aa6566700acccb50eb501d7c05892f9
kiwifruit47 <victoria.dimitrovax@icloud.com>
2024-11-23 20:46:20 +0200

    added tests for Inventory method

--- src/test/java/InventoryTest.java
+++ src/test/java/InventoryTest.java
@@ -1,44 +1,49 @@
+import org.citb408.Category;
 import org.citb408.Inventory;
 import org.citb408.Product;
-import org.citb408.Store;
 import org.citb408.exceptions.InsufficientAmountOfProductException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.mockito.Mockito.when;
+import java.math.BigDecimal;
+import java.util.HashMap;
 
 public class InventoryTest {
-    private Store storeMock;
     private Product productMock;
     private Inventory inventoryMock;
 
     @BeforeEach
     public void setUp() {
-        storeMock = Mockito.mock(Store.class);
         productMock = Mockito.mock(Product.class);
-        inventoryMock = Mockito.mock(Inventory.class);
+        HashMap<Category, BigDecimal> markup = new HashMap<>();
+        markup.put(Category.FOOD, new BigDecimal("0.10"));
+        markup.put(Category.MISCELLANEOUS, new BigDecimal("0.20"));
+        inventoryMock = new Inventory(markup, 5,50);
+    }
 
-        when(storeMock.getInventory()).thenReturn(inventoryMock);
+    //tests for isProductAvailable()
+    @Test
+    void isProductAvailable_Available() throws InsufficientAmountOfProductException {
+        inventoryMock.getAvailableProducts().put(productMock, 10);
+        assertTrue(inventoryMock.isProductAvailable(productMock, 5));
     }
 
     @Test
-    void isProductAvailable_Available() {
-        HashMap<Product, Integer> availableProducts = new HashMap<>();
-        availableProducts.put(productMock, 10);
-        when(inventoryMock.getAvailableProducts()).thenReturn(availableProducts);
-        try {
-            System.out.println(inventoryMock.getAvailableProducts());
-            System.out.println(inventoryMock.isProductAvailable(productMock, 5));
-            assertTrue(inventoryMock.isProductAvailable(productMock, 10));
+    void isProductAvailable_NotEnoughQuantity() throws InsufficientAmountOfProductException {
+        inventoryMock.getAvailableProducts().put(productMock, 10);
+        assertThrows(InsufficientAmountOfProductException.class, () -> inventoryMock.isProductAvailable(productMock, 15));
+    }
 
-        } catch (InsufficientAmountOfProductException e) {
-            fail("Exception should not be thrown");
-        }
+    @Test
+    void isProductAvailable_ProductNotInInventory() throws InsufficientAmountOfProductException {
+        assertThrows(InsufficientAmountOfProductException.class, () -> inventoryMock.isProductAvailable(productMock, 0));
+    }
+
+    //tests for calculateBasePrice
+    @Test
+    void calculateBasePrice() {
+
     }
 }

871e076d8661ada6a7afd7fbf62647555688453d
kiwifruit47 <victoria.dimitrovax@icloud.com>
2024-11-30 16:22:35 +0200

    more tests

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,7 +1,9 @@
 import org.citb408.*;
+import org.citb408.exceptions.InsufficientAmountOfMoneyException;
 import org.citb408.exceptions.ProductNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -43,5 +45,36 @@ public class RegisterTest {
     }
 
     //tests for calculateTotalPriceForProductsInClientCart()
+    @Test
+    void calculateTotalPriceForProductsInClientCart_Success() throws ProductNotFoundException {
+        Mockito.when(storeMock.getInventory()).thenReturn(inventoryMock);
+        clientMock = Mockito.mock(Client.class);
+        HashMap<Product, Integer> products = new HashMap<>();
+        products.put(productMock, 3);
+        Mockito.when(clientMock.getProductsInCart()).thenReturn(products);
+        Mockito.when(inventoryMock.calculateSellingPrice(productMock)).thenReturn(new BigDecimal("1.00"));
 
+        assertEquals(registerMock.calculateTotalPriceForProductsInClientCart(clientMock), new BigDecimal("3.00"));
+    }
+
+    //tests for makeSale()
+    @Test
+    void makeSale_Fail() throws InsufficientAmountOfMoneyException, IllegalStateException {
+        clientMock = Mockito.mock(Client.class);
+        HashMap<Product, Integer> products = new HashMap<>();
+        Mockito.when(clientMock.getProductsInCart()).thenReturn(products);
+        assertThrows(IllegalStateException.class, () -> registerMock.makeSale(clientMock, new BigDecimal("5.00")));
+    }
+
+    @Test
+    void makeSale_Fail2() throws InsufficientAmountOfMoneyException, IllegalStateException, ProductNotFoundException {
+        clientMock = Mockito.mock(Client.class);
+        Mockito.when(storeMock.getInventory()).thenReturn(inventoryMock);
+        Mockito.when(inventoryMock.calculateSellingPrice(productMock)).thenReturn(new BigDecimal("5.00"));
+        HashMap<Product, Integer> products = new HashMap<>();
+        products.put(productMock, 3);
+        Mockito.when(clientMock.getProductsInCart()).thenReturn(products);
+
+        assertThrows(InsufficientAmountOfMoneyException.class, () -> registerMock.makeSale(clientMock, new BigDecimal("5.00")));
+    }
 }

51dbc42bea8b7e8f95fa0409ff94e6450f0bf597
kiwifruit47 <victoria.dimitrovax@icloud.com>
2025-04-08 20:02:57 +0300

    added method for directory creation

--- src/main/java/org/citb408/util/Serializer.java
+++ src/main/java/org/citb408/util/Serializer.java
@@ -1,12 +1,13 @@
 package org.citb408.util;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 
 public class Serializer {
     public static <T extends Serializable> void serialize(String filePath, T item) throws IOException {
+        File dir = new File("receipts");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
         try (FileOutputStream fos = new FileOutputStream(filePath);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(item);
